4107bf7f50c0cd8a58c4d0f865c5c0b58b4a175f
Madson Santana <madson.santana96@gmail.com>
2022-10-24 11:03:24 +0000

    functions/getData: nome da propriedade

--- src/components/functions/getData.tsx
+++ src/components/functions/getData.tsx
@@ -1,12 +1,11 @@
 import axios from "axios"
-import { SetStateAction } from "react"
 import DataType from "../../interfaces/dataType"
 
-export const getData = async (setData: React.Dispatch<SetStateAction<{} | DataType>>) => {
+export const getData = async (sucessCallback: (data: DataType) => void) => {
 
     try {
         const response = await axios.get('http://localhost:3000/main')
-        setData(response.data)
+        sucessCallback(response.data)
         console.log("Resposta: ", response.data)
     } catch (e) {
         console.log(e)
